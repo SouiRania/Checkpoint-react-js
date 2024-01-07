159a2c1947fd5fcb586da7ecc0d5b8f1161c70cd
SouiRania <rania.souissi13@gmail.com>
2024-01-07 11:25:02 +0100

    aaaaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbarss from './Navbarss';
+import Heading from './Heading';
+import Carte from './Carte';
 
-export default App;
+import './Carte.css'
+ 
+ function App() {
+    return(
+        <>
+        <div className="App">
+
+        <Navbarss></Navbarss>
+        <Heading></Heading>
+        <div className='Carte'>
+         <Carte></Carte>
+         <Carte></Carte>
+         <Carte></Carte>
+         </div>
+        </div>
+    
+        </>
+
+    );
+ }
+ export default App;
+
